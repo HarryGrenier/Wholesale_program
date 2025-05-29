5ed39910f917d327a768e2720f5171a99c5b6022
Harry Grenier <hgrenier@uri.edu>
2025-05-29 17:34:08 -0400

    cleared

--- Main.py
+++
@@ -1,9 +0,0 @@
-from tkinter import Tk
-from database import init_db
-from ui import InvoiceApp
-
-if __name__ == "__main__":
-    init_db()
-    root = Tk()
-    app = InvoiceApp(root)
-    root.mainloop()
